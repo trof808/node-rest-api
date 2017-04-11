6bfc2efc1186fef5d7147460e09cc3211cbf4f4d
trof <reatrof@gmail.com>
2017-04-11 14:20:05 +0300

    logger and sessions

--- app.js
+++ app.js
@@ -2,11 +2,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const db = require('./db/db');
+const config = require('./config');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const IP = process.env.IP || 'localhost';
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'jade');
 app.use(express.static('public'));
+
+//set up sessions
+app.use(cookieParser(config.cookieSecret));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: config.cookieSecret
+}));
+
+//logger
+if (app.get('env') === 'development') {
+    app.use(require('morgan')('dev'));
+} else if ((app.get('env') === 'production')) {
+    app.use(require('express-logger')({
+        path: __dirname + '/log/request.log'
+    }));
+}
 
 app.use('/users', require('./routes/routes'));
 
